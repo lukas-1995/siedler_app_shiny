8d7f47772bec77d3237ff9c828cdf08d13dc602d
Lukas <lukas.frank@tutanota.de>
2022-02-23 22:47:50 +0100

    Try initial changes

--- ui.R
+++ ui.R
@@ -11,7 +11,7 @@ library(shinyWidgets)
 library(shinydashboard)
 library(shinyalert)
 library(odbc)
-library(RPostgreSQL)
+library(RMySQL)
 
 Sys.setenv(LANG = "en")
 Sys.setlocale("LC_ALL","English")
